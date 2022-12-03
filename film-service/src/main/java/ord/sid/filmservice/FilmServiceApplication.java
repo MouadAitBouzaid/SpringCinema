4a3a0e28d005c4c58f6a0dedbf7d91569990effe
Mouad AIT BOUZAID <77202158+MouadAitBouzaid@users.noreply.github.com>
2022-12-03 11:57:22 +0100

    0.0.1
    project creation

---
+++ film-service/src/main/java/ord/sid/filmservice/FilmServiceApplication.java
@@ -0,0 +1,13 @@
+package ord.sid.filmservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FilmServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FilmServiceApplication.class, args);
+    }
+
+}
---
+++ film-service/src/test/java/ord/sid/filmservice/FilmServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package ord.sid.filmservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FilmServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
