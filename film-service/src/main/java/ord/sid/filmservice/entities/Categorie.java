4ef17337cbb38c63ae65ce6336a73b6f6830e4df
Mouad AIT BOUZAID <77202158+MouadAitBouzaid@users.noreply.github.com>
2022-12-04 14:57:22 +0100

    3.9.8
    Billing service fixing

---
+++ film-service/src/main/java/ord/sid/filmservice/entities/Categorie.java
@@ -0,0 +1,18 @@
+package ord.sid.filmservice.entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Collection;
+@Entity
+@Data @NoArgsConstructor @AllArgsConstructor
+public class Categorie {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    @OneToMany(mappedBy = "categorie")
+    private Collection<Film> films;
+}
---
+++ film-service/src/main/java/ord/sid/filmservice/entities/Film.java
@@ -0,0 +1,22 @@
+package ord.sid.filmservice.entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Date;
+@Entity
+@Data @NoArgsConstructor @AllArgsConstructor
+public class Film {
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String titre;
+    private String description;
+    private String realisateur;
+    private Date dateSortie;
+    private double duree;
+    private String photo;
+    @ManyToOne
+    private Categorie categorie;
+}
